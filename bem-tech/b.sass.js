3f2bd3212f1c1f1355e342ffcee0ad65362a9ae8
Vitaly Harisov <vitaly@harisov.name>
2010-05-22 22:28:06 +0300

    bem технология для b.sass

---
+++ bem-tech/b.sass.js
@@ -0,0 +1,17 @@
+exports.techModule = module;
+
+exports.outFile = function (file) {
+    return '@import ' + file + '\n';
+};
+
+exports.newFileContent = function (vars) {
+    return [
+        '#{!prefix}',
+        '  &' + vars.BlockName,
+        (vars.ElemName? '    &__' + vars.ElemName : ''),
+        (vars.ModVal?
+            (vars.ElemName? '  ' : '') + '    &_' + vars.ModName + '_' + vars.ModVal :
+            ''),
+         '    ' + (vars.ElemName? '  ' : '') + (vars.ModVal? '  ' : '') + '/* ... */'
+        ].filter(function(s){ return !!s }).join('\n') + '\n';
+};
